0905faa03baa886a98bd274b8cefe8020c174732
sarath1104 <sarathchandrakumbha@gmail.com>
2022-09-12 14:48:29 +0530

    initial commit

---
+++ week2.py
@@ -0,0 +1,12 @@
+import imp
+import pandas as pd
+import numpy as np
+import re
+import nltk
+nltk.download('stopwords')
+corpus=['the sky is blue and beautiful'
+        
+        
+        
+        
+        ]
